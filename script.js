fa4cca8bba58f53d0488fa9695099915b0983329
MatthiastheHead <matthias@head-mail.com>
2025-01-02 09:57:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("clickButton");
+    button.addEventListener("click", () => {
+        alert("Du hast mich geklickt!");
+    });
+});
